a918d92276e5d76f838c6d6432346b3727b9a3b0
Bolaji Ayodeji <sirbeejay1@gmail.com>
2024-06-05 16:44:34 +0100

    refactor: update pricing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,26 @@
 module.exports = {
   content: [
     "./**/*.{html,md,liquid}",
-    "!./_quickstatic/public/*.{html,md,liquid}"
+    "!./_quickstatic/public/*.{html,md,liquid}",
   ],
   safelist: [
-    'h-screen',
-    'h-full',
-    'w-screen',
-    'w-full',
-    'h-lvh',
-    'lg:grid-cols-4',
-    'text-md',
-    'pl-6',
-    'pl-9',
-    'pl-12',
+    "h-screen",
+    "h-full",
+    "w-screen",
+    "w-full",
+    "h-lvh",
+    "lg:grid-cols-4",
+    "text-md",
+    "pl-6",
+    "pl-9",
+    "pl-12",
   ],
   theme: {
+    // colors: {
+    //   atblue: "#0068ff",
+    //   atyellow: "#ffff00",
+    //   atlightblue: "#567bd6",
+    // },
     extend: {},
   },
   variants: {
@@ -26,33 +31,36 @@ module.exports = {
     require("daisyui"),
     // ...
   ],
-  darkMode: ['class', '[data-theme="sunset"]'],
+  darkMode: ["class", '[data-theme="sunset"]'],
   // daisyUI config (optional - here are the default values)
   daisyui: {
     // themes: true, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    themes: ["emerald", {
-      sunset: {
-        "color-scheme": "dark",
-        "primary": "#FF865B",
-        "secondary": "#377cfb",
-        "secondary-content": "#fff",
-        "accent": "#B387FA",
-        "neutral": "oklch(26% 0.019 237.69)",
-        "neutral-content": "oklch(70% 0.019 237.69)",
-        "base-100": "#0B0C14",
-        "base-200": "oklch(20% 0.019 237.69)",
-        "base-300": "oklch(18% 0.019 237.69)",
-        "base-content": "rgb(201, 211, 238)",
-        "info": "#89e0eb",
-        "success": "#addfad",
-        "warning": "#f1c891",
-        "error": "#ffbbbd",
-        "--rounded-box": "1.2rem",
-        "--rounded-btn": "0.8rem",
-        "--rounded-badge": "0.4rem",
-        "--tab-radius": "0.7rem",
-      }
-    }],
+    themes: [
+      "emerald",
+      {
+        sunset: {
+          "color-scheme": "dark",
+          primary: "#FF865B",
+          secondary: "#377cfb",
+          "secondary-content": "#fff",
+          accent: "#B387FA",
+          neutral: "oklch(26% 0.019 237.69)",
+          "neutral-content": "oklch(70% 0.019 237.69)",
+          "base-100": "#0B0C14",
+          "base-200": "oklch(20% 0.019 237.69)",
+          "base-300": "oklch(18% 0.019 237.69)",
+          "base-content": "rgb(201, 211, 238)",
+          info: "#89e0eb",
+          success: "#addfad",
+          warning: "#f1c891",
+          error: "#ffbbbd",
+          "--rounded-box": "1.2rem",
+          "--rounded-btn": "0.8rem",
+          "--rounded-badge": "0.4rem",
+          "--tab-radius": "0.7rem",
+        },
+      },
+    ],
     darkTheme: "sunset", // name of one of the included themes for dark mode
     // darkTheme: false,
     base: true, // applies background color and foreground color for root element by default
