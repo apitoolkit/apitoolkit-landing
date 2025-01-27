b08727c563d59c23386cdd7750035409636a13e3
Anthony Alaribe <anthonyalaribe@gmail.com>
2025-01-27 17:15:24 +0100

    new landing starter

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,12 +16,111 @@ module.exports = {
     "pl-12",
   ],
   theme: {
-    // colors: {
-    //   atblue: "#0068ff",
-    //   atyellow: "#ffff00",
-    //   atlightblue: "#567bd6",
-    // },
-    extend: {},
+    extend: {
+      colors: {
+        background: {
+          alternate: "#F5F6FA",
+          base: "#FBFCFD",
+          inverse: "#12131A",
+          overlay: "#FBFCFD",
+          raised: "#FBFCFD",
+          sunken: "#F5F6FA",
+        },
+        fill: {
+          brand: {
+            strong: "#0068FF",
+            weak: "#0068FF0D",
+          },
+          disabled: "#0011661A",
+          error: {
+            strong: "#C73A3A",
+            weak: "#FF4A4A0D",
+          },
+          hover: "#0015800A",
+          information: {
+            strong: "#1A74A8",
+            weak: "#26B0FF0D",
+          },
+          inverse: {
+            disabled: "#FFFFFF1F",
+            weak: "#FFFFFF0F",
+          },
+          overlay: "#000D4D73",
+          press: "#0011661A",
+          selected: "#0068FF",
+          success: {
+            strong: "#067A57",
+            weak: "#0ACC920D",
+          },
+          warning: {
+            strong: "#8F6C1A",
+            weak: "#FFC02E0D",
+          },
+          weak: "#0015800A",
+          weaker: "#00158005",
+          white: "#FFFFFF",
+          yellow: "#FEC62E",
+        },
+        icon: {
+          brand: "#0068FFCC",
+          disabled: "#0011661A",
+          error: "#C73A3ACC",
+          information: "#1A74A8CC",
+          inverse: {
+            DEFAULT: "#FFFFFF99",
+            disabled: "#FFFFFF1F",
+            strong: "#FFFFFF",
+          },
+          neutral: "#000D4D73",
+          success: "#067A57CC",
+          warning: "#8F6C1ACC",
+        },
+        stroke: {
+          brand: {
+            strong: "#0068FFCC",
+            weak: "#0068FF33",
+          },
+          disabled: "#0011661A",
+          error: {
+            strong: "#C73A3ACC",
+            weak: "#C73A3A24",
+          },
+          focus: "#0068FF",
+          information: {
+            strong: "#1A74A8CC",
+            weak: "#1A74A833",
+          },
+          inverse: {
+            disabled: "#FFFFFF1F",
+            strong: "#FFFFFF99",
+            weak: "#FFFFFF1F",
+          },
+          selected: "#0068FF",
+          strong: "#000D4D73",
+          success: {
+            strong: "#067A57CC",
+            weak: "#067A5733",
+          },
+          warning: {
+            strong: "#8F6C1ACC",
+            weak: "#8F6C1A33",
+          },
+          weak: "#0011661A",
+        },
+        text: {
+          brand: "#0068FF",
+          disabled: "#000D4D73",
+          error: "#C73A3A",
+          inverse: {
+            disabled: "#FFFFFF1F",
+            strong: "#FFFFFF",
+            weak: "#FFFFFFC7",
+          },
+          strong: "#000626E6",
+          weak: "#000833A6",
+        }
+      },
+    },
   },
   variants: {
     extend: {},
